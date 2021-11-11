02d8d6079920530eb3a097c904d3b05c2389b33b
abhishek-netizen <abhishekwithu101@gmail.com>
2021-11-11 18:14:24 +0530

    updated changes

--- Functions/test.ts
+++ Functions/test.ts
@@ -2,7 +2,7 @@ import { SamlConsolePrincipal } from "@aws-cdk/aws-iam"
 
 export const handler=async function(event:any,callback:any){
     try {
-        
+        console.log("hello");
         return {
             statusCode:200,
             body:"success"
